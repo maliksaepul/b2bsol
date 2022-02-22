48093e48e26c4179ac473419e8f1e9e58ba8c402
maliksaepul <malik@inspigo.id>
2022-02-22 12:13:41 +0700

    chore : fix sidebar

--- components/molecules/appbar/index.jsx
+++ components/molecules/appbar/index.jsx
@@ -6,15 +6,19 @@ import Humberger from '@/components/molecules/humberger'
 import PropTypes from 'prop-types'
 import Sidebar from '@/containers/components/bars/Sidebar'
 import { disableScroll } from '@/utils/helpers'
+import { mediaMaxWidth, MobileBreakpoint } from '@/utils/breakpoint'
 
 const Appbar = ({ activeSidebar, sidebarState, account, path }) => {
-    if (sidebarState) {
+    if (mediaMaxWidth(MobileBreakpoint) && sidebarState) {
         disableScroll.on()
+    } else {
+        disableScroll.off()
+    }
+    if (sidebarState) {
         return (
             <Sidebar username={account.name} usercover={account.cover?.url} />
         )
     } else {
-        disableScroll.off()
         return (
             <div className={styles.appbar}>
                 <Logo link={path.path} />
